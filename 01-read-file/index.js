0afc8e6c45bc5b4ca2fde96255db3f6446c4d8d3
Boris Kortunov <boris.kortunov@gmail.com>
2022-05-18 16:19:19 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const readableStream = fs.createReadStream(
+    path.join(__dirname, 'text2.txt'),
+    'utf-8',
+    );
+
+    readableStream.on('data', chunk => console.log(chunk));
+    readableStream.on('error', error => console.log('Error', error.message));
