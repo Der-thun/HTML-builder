d0bc01f3a4524d85a5f42a68a7683525c4a59796
Boris Kortunov <boris.kortunov@gmail.com>
2022-05-22 16:16:38 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir () {
+    const dirPathFrom = path.join(__dirname, 'files');
+    const dirPathTo = path.join(__dirname, 'files-copy');
+
+    fs.open(dirPathTo, err => {
+        if (err) {
+            fs.mkdir(dirPathTo, err => {
+                if (err) throw err;
+            });           
+        }    
+
+        fs.readdir(dirPathTo, (err, files) => {
+            files.forEach(file => {
+                const fileTo = path.join(dirPathTo, file);                    
+                fs.promises.unlink(fileTo)
+            })
+        })
+
+        fs.readdir(dirPathFrom, (err, files) => {
+            files.forEach(file => {
+                const fileFrom = path.join(dirPathFrom, file);
+                const fileTo = path.join(dirPathTo, file);
+                fs.promises.copyFile(fileFrom, fileTo)
+            })
+        })
+
+    })
+}
+
+copyDir();
